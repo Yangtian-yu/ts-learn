616f10960228ed90725ad59d1a0d047b6711e189
yangtianyu <535173883@qq.com>
2021-12-18 00:53:43 +0800

    day 1

---
+++ ts_in_action/src/index.ts
@@ -0,0 +1,8 @@
+import "./datatype";
+import "./enum";
+import "./interfaceForObject";
+import "./interfaceForFun";
+import "./function";
+
+let hello: string = "Hello TypeScript";
+document.querySelectorAll(".app")[0].innerHTML = hello;
---
+++ ts_in_action/src/interfaceForFun.ts
@@ -0,0 +1,28 @@
+let add1: (x: number, y: number) => number;
+
+interface Add {
+  (x: number, y: number): number;
+}
+
+type ADD = (x: number, y: number) => number;
+
+let add2: ADD = (a, b) => a + b;
+
+interface Lib {
+  (): void;
+  version: string;
+  doSomething(): void;
+}
+
+function getLib() {
+  let lib: Lib = (() => {}) as Lib;
+  lib.version = "1.0";
+  lib.doSomething = () => {
+    console.log("doSomething");
+  };
+  return lib;
+}
+
+let lib1 = getLib();
+lib1.doSomething();
+let lib2 = getLib();
