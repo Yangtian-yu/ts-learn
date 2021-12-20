620d700ef6b2c70741295eadbc5cb08e31725404
yangtianyu <535173883@qq.com>
2021-12-21 01:45:17 +0800

    .

--- ts_in_action/src/index.ts
+++ ts_in_action/src/index.ts
@@ -3,6 +3,9 @@ import "./enum";
 import "./interfaceForObject";
 import "./interfaceForFun";
 import "./function";
+import "./class";
+import "./abstractClass";
+import "./class-inerface";
 
 let hello: string = "Hello TypeScript";
 document.querySelectorAll(".app")[0].innerHTML = hello;
