b3b92427d6f14ba5878538e96feca0248f0b917d
yangtianyu <535173883@qq.com>
2022-08-20 16:59:02 +0800

    011 TupleToObject

--- TDL-TS/00011-easy-tuple-to-object/template.ts
+++ TDL-TS/00011-easy-tuple-to-object/template.ts
@@ -1,4 +1,4 @@
-export type TupleToObject<T extends readonly (string | symbol | number)[]> = {
+export type TupleToObject<T extends readonly (string | number | symbol)[]> = {
   [P in T[number]]: P;
 };
 
@@ -8,8 +8,12 @@ type r = TupleToObject<typeof tuple>;
 
 //js
 function TupleToObject(array) {
+  //1.array 里面的key 是不是 string 类型 或者是不是number 或者是不是symbol
+  //error
   const obj = {};
-
+  array.forEach((element) => {
+    obj[element] = element;
+  });
   return obj;
 }
 
--- TDL-TS/00011-easy-tuple-to-object/test-cases.ts
+++ TDL-TS/00011-easy-tuple-to-object/test-cases.ts
@@ -3,6 +3,19 @@ import { TupleToObject } from "./template";
 
 const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
 
+type r = typeof tuple;
+// tuple[0] = "111"; // as const
+let a: r = ["tesla", "model 3", "model X", "model Y"];
+
+//1.字面量类型
+let str = "123";
+type s = typeof str;
+str = "11111"; //可修改
+
+const strConst = "3213213"; //常量
+type str = typeof strConst;
+// let b: str = "99999"; //不可修改
+
 type cases = [
   Expect<
     Equal<
@@ -20,5 +33,7 @@ type cases = [
 // @ts-expect-error
 type error = TupleToObject<[[1, 2], {}]>;
 
+//const let js 世界
+//type interface type 世界
 //1.typeof
 //2.字面量类型 as const
