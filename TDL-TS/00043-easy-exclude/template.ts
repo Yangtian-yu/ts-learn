22045213e30404577e244c8054386df3982e6420
yangtianyu <535173883@qq.com>
2022-08-22 23:05:53 +0800

    00043-easy-exclude

--- TDL-TS/00043-easy-exclude/template.ts
+++ TDL-TS/00043-easy-exclude/template.ts
@@ -1,1 +1,23 @@
-type MyExclude<T, U> = any
+type MyExclude<T, U> = T extends U ? never : T;
+//js
+function exclude(T: any[], U: any[]) {
+  const result = [];
+  for (let i = 0; i < T.length; i++) {
+    const t = T[i];
+    //   let boo = false;
+    //   for (let j = 0; j < U.length; j++) {
+    //     const u = U[j];
+    //     if (t === u) {
+    //       boo = true;
+    //     }
+    //   }
+    //   if (!boo) {
+    //     result.push(t);
+    //   }
+    if (!U.includes(t)) {
+      result.push(t);
+    }
+  }
+
+  return result;
+}
--- TDL-TS/00043-easy-exclude/test-cases.ts
+++ TDL-TS/00043-easy-exclude/test-cases.ts
@@ -1,7 +1,11 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type t1 = Exclude<"a" | "b" | "c", "a">;
 
 type cases = [
-  Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a'>, 'b' | 'c'>>,
-  Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a' | 'b'>, 'c'>>,
-  Expect<Equal<MyExclude<string | number | (() => void), Function>, string | number>>,
-]
+  Expect<Equal<MyExclude<"a" | "b" | "c", "a">, "b" | "c">>,
+  Expect<Equal<MyExclude<"a" | "b" | "c", "a" | "b">, "c">>,
+  Expect<
+    Equal<MyExclude<string | number | (() => void), Function>, string | number>
+  >
+];
