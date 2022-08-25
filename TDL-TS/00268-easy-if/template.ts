4de08f39f8d46426e316bb67cb50a92f496678a7
yangtianyu <535173883@qq.com>
2022-08-25 10:32:26 +0800

    00268-easy-if

--- TDL-TS/00268-easy-if/template.ts
+++ TDL-TS/00268-easy-if/template.ts
@@ -1,1 +1,11 @@
-type If<C, T, F> = any
+type If<C extends boolean, T, F> = C extends true ? T : F;
+
+//js
+function If(C, T, F) {
+  return C ? T : F;
+}
+
+//知识点
+//类型兼容性 | 分配规则
+//null 严格模式和非严格模式的区别
+//1. https://www.typescriptlang.org/docs/handbook/type-compatibility.html#handbook-content
